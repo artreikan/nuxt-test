7854b238e2ef96ce56879ec07917e587201ed598
Roman <romagluten@gmail.com>
2020-09-17 15:04:37 +0300

    initial commit

---
+++ pages/users/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>User: {{ id }}</h1>
+</template>
+
+<script>
+export default {
+  computed: {
+    id() {
+      return this.$route.params.id;
+    }
+  },
+  validate({ params }) {
+    // Must be a number
+    return /^\d+$/.test(params.id);
+  },
+};
+</script>
